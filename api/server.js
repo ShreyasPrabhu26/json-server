5d157ab1fd0e8cb964dd5c5f3d1daa4d5845ab74
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2023-11-11 12:46:49 +0530

    Change Vercel

--- api/server.js
+++ api/server.js
@@ -4,15 +4,15 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 
 // Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('data/questions.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('data/questions.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
 
 // Comment out to allow write operations
-const router = jsonServer.router('questions.json')
+// const router = jsonServer.router('questions.json')
 
 const middlewares = jsonServer.defaults()
 
